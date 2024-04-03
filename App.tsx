2778573e9521a6665c9dd957e800e1daa834a596
Rojimme <merodriguezji@est.utn.ac.cr>
2024-04-03 10:08:01 -0600

    agregando cámara al teléfono

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+		import {View} from 'react-native';
+		//import ValiUser from './src/components/users/fb_Valiusers';
+			//import RegisterUser from './src/components/users/fb_Registeruser';
+			//import LoginUser from './src/components/users/fb_Loginuser';
+			//import LogoutUser from './src/components/users/fb_Logoutuser';
+			import InsPicture from './src/components/collections/galleria/fb_Inspicture';
+		
+			const App = () => {
+			  return (
+				<View>
+				  <InsPicture />
+				</View>
+			  );
+			};
+
+		export default App;
